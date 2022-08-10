c9bcff4ab7c26b70e92c470bcba80f5b9a973a54
searchsneh <sneh.shrivastava@saveo.in>
2022-08-10 15:41:44 +0530

    config change for kore-ai

--- app.js
+++ app.js
@@ -1,100 +1,100 @@
-var express     = require('express');  
-var mongoose    = require('mongoose');  
-var multer      = require('multer');  
-var path        = require('path');  
-var csvModel    = require('./models/medicineModel');  
-var csv         = require('csvtojson');  
-var bodyParser  = require('body-parser');  
+var express = require('express');
+var mongoose = require('mongoose');
+var multer = require('multer');
+var path = require('path');
+var csvModel = require('./models/medicineModel');
+var csv = require('csvtojson');
+var bodyParser = require('body-parser');
 const uuid = require("uuid");
-var storage = multer.diskStorage({  
-    destination:(req,file,cb)=>{  
-      cb(null,'./public/uploads');  
-    },  
-    filename:(req,file,cb)=>{  
-      cb(null,file.originalname);  
-    }  
-});  
-var uploads = multer({storage:storage});  
+var storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+var uploads = multer({ storage: storage });
 
-mongoose.connect('mongodb+srv://user007:alwaysme@cluster0.itp2v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{useNewUrlParser:true})  
-  .then(()=>console.log('connected to db'))  
-  .catch((err)=>console.log(err));  
+mongoose.connect('mongodb+srv://user007:alwaysme@cluster0.itp2v.mongodb.net/kore-ai?retryWrites=true&w=majority', { useNewUrlParser: true })
+  .then(() => console.log('connected to db'))
+  .catch((err) => console.log(err));
 
-var app = express();  
+var app = express();
 
-app.set('view engine','ejs');  
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended:false}));  
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.resolve(__dirname,'public')));  
+app.use(express.static(path.resolve(__dirname, 'public')));
 
-app.get('/',(req,res)=>{  
-  res.render('index');        
-});  
-app.post('/uploadCSV',uploads.single('csv'),(req,res)=>{  
-  csv()  
-  .fromFile(req.file.path)  
-  .then((jsonObj)=>{  
-    console.log('json object created');
-    csvModel.insertMany(jsonObj,(err,data)=>{  
-    if(err){  
-      console.log(err);  
-      res.send({err});
-    }else{  
-      res.redirect('/');  
-    }  
-    });  
-  });  
-});  
-app.get('/getMedicineDetails', (req,res)=>{
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.post('/uploadCSV', uploads.single('csv'), (req, res) => {
+  csv()
+    .fromFile(req.file.path)
+    .then((jsonObj) => {
+      console.log('json object created');
+      csvModel.insertMany(jsonObj, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.send({ err });
+        } else {
+          res.redirect('/');
+        }
+      });
+    });
+});
+app.get('/getMedicineDetails', (req, res) => {
   let c_unique_id = req.body.c_unique_id;
-  csvModel.find({'c_unique_code':c_unique_id},function(err,data){
+  csvModel.find({ 'c_unique_code': c_unique_id }, function (err, data) {
     res.setHeader('Content-Type', 'application/json');
-    if(err)
+    if (err)
       res.send(err);
     else
-      res.send({'data':data});
+      res.send({ 'data': data });
   });
   console.log(c_unique_id);
 });
 
-app.get('/searchMedicine',(req,res)=>{
+app.get('/searchMedicine', (req, res) => {
   let name = req.body.name;
-  csvModel.find({ "c_name": { $regex: '.*' + name + '.*' }},'c_name',function(err,data){
+  csvModel.find({ "c_name": { $regex: '.*' + name + '.*' } }, 'c_name', function (err, data) {
     res.setHeader('Content-Type', 'application/json');
-    if(err)
+    if (err)
       res.send(err);
     else
-      res.send({'data':data});
+      res.send({ 'data': data });
   });
 });
 
-app.put('/placeorder',async (req,res)=> {
+app.put('/placeorder', async (req, res) => {
   let c_unique_id = req.body.c_unique_id;
   let quantity = req.body.quantity;
   let c_name = req.body.c_name;
-  let data = await csvModel.findOne({ "c_name": c_name, "c_unique_code": c_unique_id});
+  let data = await csvModel.findOne({ "c_name": c_name, "c_unique_code": c_unique_id });
   let orignal_quantity = data.n_balance_qty;
   let finalData;
-  if(orignal_quantity>quantity){
-    let temp = orignal_quantity-quantity;
-    finalData = await csvModel.findOneAndUpdate({ "c_name": c_name, "c_unique_code": c_unique_id},{"n_balance_qty":temp});
+  if (orignal_quantity > quantity) {
+    let temp = orignal_quantity - quantity;
+    finalData = await csvModel.findOneAndUpdate({ "c_name": c_name, "c_unique_code": c_unique_id }, { "n_balance_qty": temp });
     res.setHeader('Content-Type', 'application/json');
-    finalData = await csvModel.findOne({ "c_name": c_name, "c_unique_code": c_unique_id});
-    if(!finalData){
-      res.send({"err": "didnt find any data"});
+    finalData = await csvModel.findOne({ "c_name": c_name, "c_unique_code": c_unique_id });
+    if (!finalData) {
+      res.send({ "err": "didnt find any data" });
     }
     else
-      res.send({"oder_id":uuid.v4()});
+      res.send({ "oder_id": uuid.v4() });
   }
-  else{
-    res.send({err:"error in quantity"});
+  else {
+    res.send({ err: "error in quantity" });
   }
 
-  
-  
+
+
 });
 //assign port  
-var port = process.env.PORT || 3000;  
-app.listen(port,()=>console.log('server run at port '+port));  
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log('server run at port ' + port));  
